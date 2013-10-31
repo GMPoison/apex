7cf7e98ac2cbe79fd0e6da30ed00e7cd42a920b3
GMPoison <Warcraft365@yahoo.com>
2013-10-30 23:59:48 -0400

    ton_of_bricks_for

--- APEX/src/ton_of_bricks/ton_of_bricks.java
+++ APEX/src/ton_of_bricks/ton_of_bricks.java
@@ -4,16 +4,42 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class ton_of_bricks extends Applet{
-		
-		private static final long serialVersionUID = 1L;
+	
+   private static final long serialVersionUID = 1L;
+  
+   public static final int START_X = 0;
+   public static final int START_Y = 0;
+   public static final int BRICK_WIDTH = 32;
+   public static final int BRICK_HEIGHT = 16;
+   public static final int GAP = 4;
+   public static final int DISPLAY_WIDTH = 600;
+   public static final int DISPLAY_HEIGHT = 600;
 
-			public void init(){
-				
-			}// end Init
-			public void paint(Graphics g){
-				
-			} //end Paint
-			
-	} //end ActionListener
+   public void paint(Graphics g){
+	   
+      int x = START_X;
+      int y = START_Y;
 
+      resize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+      setBackground(Color.darkGray);
+      g.setColor(Color.red);
 
+      for (int row = 1; row <= DISPLAY_HEIGHT; row++){
+
+         for (int col = 1; col <= DISPLAY_WIDTH; col++){ 
+        	 
+            g.fillRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
+            x += BRICK_WIDTH + GAP;
+            
+         }
+
+         if (row%2 == 0)
+             x = START_X;
+          else
+             x = START_X - BRICK_WIDTH/2 - GAP/2;
+
+         y += BRICK_HEIGHT + GAP;
+
+      }
+   }
+}
