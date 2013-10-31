08860d8e55cdc5f7c5f496e7fc36257f7eb57be2
gmpoison <warcraft365@yahoo.com>
2013-10-31 11:24:28 -0400

    Added comments, increased readability.

--- APEX/src/ton_of_bricks/Ton_of_bricks_for.java
+++ APEX/src/ton_of_bricks/Ton_of_bricks_for.java
@@ -9,37 +9,38 @@ public class Ton_of_bricks_for extends Applet{
   
    public static final int START_X = 0;
    public static final int START_Y = 0;
-   public static final int BRICK_WIDTH = 30;
-   public static final int BRICK_HEIGHT = 15;
-   public static final int GAP = 5;
+   public static final int BRICK_WIDTH = 32;
+   public static final int BRICK_HEIGHT = 16;
    public static final int DISPLAY_WIDTH = 600;
    public static final int DISPLAY_HEIGHT = 600;
+   public static final int GAP = 4;
 
    public void paint(Graphics g){
 	   
       int x = START_X;
       int y = START_Y;
+      
+      setBackground(Color.BLACK);
+      g.setColor(Color.MAGENTA); 
 
-      resize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-      setBackground(Color.darkGray);
-      g.setColor(Color.red);
+      for (int row = 1; row <= DISPLAY_WIDTH; row++){ //while row is less than windows width, increase rows
 
-      for (int row = 1; row <= DISPLAY_HEIGHT; row++){
-
-         for (int col = 1; col <= DISPLAY_WIDTH; col++){ 
+         for (int col = 1; col <= DISPLAY_HEIGHT; col++){ //while col is less than windows height, increase cols 
         	 
-            g.fillRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
-            x += BRICK_WIDTH + GAP;
+        	 resize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+        	 g.fillRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
+            
+            x += BRICK_WIDTH + GAP; //generates columns
             
          }
 
          if (row%2 == 0)
              x = START_X;
           else
-             x = START_X - BRICK_WIDTH/2 - GAP/2;
+        	  x = START_X - BRICK_WIDTH/2 - GAP/2; //offsets bricks  
 
-         y += BRICK_HEIGHT + GAP;
+         y += BRICK_HEIGHT + GAP; //generates rows
 
-      }
-   }
-}
+      } //end for(rows)
+   } //end pain
+} //public class ton_of_bricks_for
