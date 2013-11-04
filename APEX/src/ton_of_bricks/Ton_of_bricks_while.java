3ac53d611c64d8fc615348cdd249e2898ce807be
gmpoison <warcraft365@yahoo.com>
2013-11-04 13:34:36 -0500

    While updated: First column working

--- APEX/src/ton_of_bricks/Ton_of_bricks_while.java
+++ APEX/src/ton_of_bricks/Ton_of_bricks_while.java
@@ -2,30 +2,48 @@ package ton_of_bricks;
 
 import java.applet.Applet;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
 public class Ton_of_bricks_while extends Applet{
+	
+   private static final long serialVersionUID = 1L;
+  
+   public static final int START_X = 0;
+   public static final int START_Y = 0;
+   public static final int BRICK_WIDTH = 30;
+   public static final int BRICK_HEIGHT = 15;
+   public static final int DISPLAY_WIDTH = 600;
+   public static final int DISPLAY_HEIGHT = 600;
+   public static final int GAP = 5;
 
-		private static final long serialVersionUID = 1L;
+   	public void paint(Graphics g){
+	   
+   	   int x = START_X;
+   	   int y = START_Y;
+   		     
+   	   setBackground(Color.BLACK);
+   	   g.setColor(Color.MAGENTA); 
+      
+   	   int row = 1;
+   	   int col = 1;
+   	   
+   	   while (row <= DISPLAY_WIDTH){ //while row is less than windows width, increase rows
+   		   row++;
+   			
+   	   		while (col <= DISPLAY_HEIGHT) //while col is less than windows height, increase cols 
+   	   			col++;         	 
+   					
+   	   			g.fillRect (x, y, BRICK_WIDTH, BRICK_HEIGHT); //drawing bricks
+            
+   				x += BRICK_WIDTH + GAP; //generates columns
+   	   		
 
-			public static final int DISPLAY_WIDTH = 600;
-			public static final int DISPLAY_HEIGHT = 600;
-		
-			public void init(){
-				
-				
-			} // end init
-			public void paint(Graphics g){
-				
-				resize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-								
-			} //end paint
-			
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-}
+   			if (row%2 == 0)
+   				x = START_X;
+   			else
+   				x = START_X - BRICK_WIDTH/2 - GAP/2; //offsets bricks  
 
-
+   				y += BRICK_HEIGHT + GAP; //generates rows
+   	   		
+   	   } //end for(rows)
+   } //end pain
+}//public class ton_of_bricks_for
